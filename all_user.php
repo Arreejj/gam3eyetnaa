e97801178c3ef9eab43e4b88e2c7ec7aa44b2af1
shahd soliman <shahd2101760@miuegypt.edu.eg>
2024-05-20 00:42:44 +0300

    search

--- all_user.php
+++ all_user.php
@@ -1,7 +1,18 @@
 <?php
 
 include 'partials/_dbconnect.php';
-$sql = "SELECT * FROM `users`";
+
+// Initialize an empty variable for the search term
+$searchTerm = '';
+
+// Check if a search term is submitted
+if (isset($_POST['search'])) {
+    $searchTerm = mysqli_real_escape_string($conn, $_POST['search']);
+    $sql = "SELECT * FROM `users` WHERE `name` LIKE '%$searchTerm%'";
+} else {
+    $sql = "SELECT * FROM `users`";
+}
+
 $result = mysqli_query($conn, $sql);
 
 ?>
@@ -15,20 +26,59 @@ $result = mysqli_query($conn, $sql);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
-    <link rel="preconnect" href="https://fonts.gstatic.com"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/navbar.css">
     <link rel="stylesheet" href="css/footer.css">
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/table.css">
     <title>ALL USERS</title>
+
+    <style>
+        .search-form {
+            display: flex;
+            justify-content: center;
+            margin: 20px 0;
+        }
+
+        .search-form input[type="text"] {
+            padding: 10px;
+            width: 300px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            font-size: 16px;
+        }
+
+        .search-form button {
+            padding: 10px 20px;
+            border: none;
+            background-color: grey;
+            color: white;
+            border-radius: 5px;
+            cursor: pointer;
+            margin-left: 10px;
+        }
+
+        .search-form button:hover {
+            background-color: black;
+        }
+    </style>
 </head>
 
 <body>
 
     <div class="cover"></div>
-    
+
     <h1>ALL &nbsp; USERS</h1>
+
+    <!-- Search Form -->
+    <div class="search-form">
+        <form method="POST" action="">
+            <input type="text" name="search" placeholder="Search by name" value="<?php echo htmlspecialchars($searchTerm); ?>">
+            <button type="submit">Search</button>
+        </form>
+    </div>
+
     <div class="all_users" style="height: 500px;">
         <table>
             <tr>
